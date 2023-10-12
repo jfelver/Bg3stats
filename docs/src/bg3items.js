2932b933d1c1085fd1da7264697b5b6a541ce90b
Justin Felver <justinfelver@gmail.com>
2023-10-12 13:49:14 -0400

    Updating data parsing and page format

--- docs/src/bg3items.js
+++ docs/src/bg3items.js
@@ -1,11 +1,22 @@
 // Load the JSON file
 let bg3items = {};
+let availableFields = [];
 
 fetch("https://jfelver.github.io/Bg3stats/src/stats.json")
   .then((response) => response.json())
   .then((data) => {
     bg3items = data;
+    for(item in bg3items) {
+      for(field in bg3items[item]) {
+        if(!availableFields.includes(field)) {
+          availableFields.push(field);
+        }
+      }
+    }
+    availableFields.sort();
   })
   .catch((error) => {
     console.error("Error fetching JSON file:", error);
   });
+
+
